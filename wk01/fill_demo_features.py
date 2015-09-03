fa58380c43d69757050c1c27eda233b0a5101748
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-09-03 20:48:00 +0900

    x 축 점 수를 1800 으로 늘림 그래프를 더 부드럽게 표시

--- wk01/fill_demo_features.py
+++ wk01/fill_demo_features.py
@@ -10,7 +10,7 @@ In addition to the basic fill plot, this demo shows a few optional feastures:
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 5.0, 100)
+x = np.linspace(0, 5.0, 5*360)
 y1 = np.sin(  2*np.pi*x)
 y2 = np.sin(3*2*np.pi*x)
 plt.fill(x, y1, 'b',
