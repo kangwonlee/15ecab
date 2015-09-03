37e9eacb6144b449b6b7853c30cc7468dfacb823
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-09-03 20:44:26 +0900

    sine 함수 인자(argument)에 2pi를 곱함 주기를 1로 맞추기 위함 x가 0에서 1까지 변할 때 sine 함수 y1 주기가 5번 반복 그래프가 표족하게 보임

--- wk01/fill_demo_features.py
+++ wk01/fill_demo_features.py
@@ -11,8 +11,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 5.0, 100)
-y1 = np.sin(x)
-y2 = np.sin(3*x)
+y1 = np.sin(  2*np.pi*x)
+y2 = np.sin(3*2*np.pi*x)
 plt.fill(x, y1, 'b',
          x, y2, 'r',
          alpha=0.3)
