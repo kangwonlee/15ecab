81f4fe2beb49fa5536c2d093dee76b1b58328db7
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-09-03 20:37:40 +0900

    x 축 범위를 2 pi 에서 5로 변경; 그래프 모양이 달라짐. 채워진 영역이 사선으로 보임

--- wk01/fill_demo_features.py
+++ wk01/fill_demo_features.py
@@ -10,7 +10,7 @@ In addition to the basic fill plot, this demo shows a few optional feastures:
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(0, 2 * np.pi, 100)
+x = np.linspace(0, 5.0, 100)
 y1 = np.sin(x)
 y2 = np.sin(3*x)
 plt.fill(x, y1, 'b',
