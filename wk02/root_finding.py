790a7950b48e540bfb677f7bcf8918ce3efcb819
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-09-10 18:21:15 +0900

    sequential() 함수 입력

--- wk02/root_finding.py
+++ wk02/root_finding.py
@@ -1,2 +1,14 @@
 def sequential(f, x0):
     xi = float(x0)
+    delta_x = 1e-6
+    counter = 0
+    while True:
+        fi = f(xi)
+        if abs(fi) < epsilon:
+            break
+        xi += delta_x
+        counter += 1
+    print "seq_counter =", counter
+    return xi
+# end of sequential()
+
