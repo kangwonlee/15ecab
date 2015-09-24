e231d6e333127642e665ed9fd96de1a574bb0333
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-09-24 18:58:24 +0900

    rect0() : 함수 내용 입력

--- wk04/num_int.py
+++ wk04/num_int.py
@@ -18,3 +18,28 @@ def rect0(f, x0, x1, n=100):
     ------
     Numerical integration of function f(x) in interval [x0, x1]
     """
+    # calculate x interval
+    delta_x = (float(x1) - float(x0)) / n
+
+    # generate list x
+    # k = 0, 1, 2, ..., (n-1)
+    # list of mid point of each rectangle
+    x = [x0 + delta_x * (0.5 + k ) for k in xrange(n)]
+
+    # integration result
+    result = 0.0
+
+    # k loop
+    # k = 0, 1, 2, ..., (n-1)
+    for k in xrange(n):
+        # k-th x
+        xk = x[k]
+        # k-th area
+        F_k = f(xk) * delta_x
+        # accumulate to integration result
+        result += F_k
+    # end k loop
+
+    # return integration result
+    return result
+# end of function rect0()
