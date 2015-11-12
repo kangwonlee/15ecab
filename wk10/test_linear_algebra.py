3dec749f57cc045993a9d9d81caf75b7f4642278
KangWon LEE <kangwon.lee@kpu.ac.kr>
2015-11-12 17:50:09 +0900

    test_linear_algebra.py : 벡터 내적 test 추가

---
+++ wk10/test_linear_algebra.py
@@ -0,0 +1,47 @@
+import unittest
+import linear_algebra as la
+
+
+class TestLinearAlgebra(unittest.TestCase):
+    def test_dot_01(self):
+        a = [1.0, 1.0]
+        b = [1.0, 1.0]
+        c = la.dot(a, b)
+        expected = 2.0
+
+        self.assertAlmostEqual(c, expected)
+
+    def test_dot_02(self):
+        a = [1.0, 1.0]
+        b = [1.0, 0.0]
+        c = la.dot(a, b)
+        expected = 1.0
+
+        self.assertAlmostEqual(c, expected)
+
+    def test_dot_03(self):
+        a = [1.0, 1.0]
+        b = [0.0, 1.0]
+        c = la.dot(a, b)
+        expected = 1.0
+
+        self.assertAlmostEqual(c, expected)
+
+    def test_dot_04(self):
+        a = [1.0, 2.0, 3.0]
+        b = [0.0, 1.0, 0.0]
+        c = la.dot(a, b)
+        expected = 2.0
+
+        self.assertAlmostEqual(c, expected)
+
+    def test_dot_05(self):
+        a = [ 3.0, 4.0]
+        b = [-4.0, 3.0]
+        c = la.dot(a, b)
+        expected = 0.0
+
+        self.assertAlmostEqual(c, expected)
+
+if "__main__" == __name__:
+    unittest.main()
